9447bafc9d7b24f518ccf7d70b20583bf27098dc
muhdshahan <shahuuraff@gmail.com>
2025-09-03 20:21:16 +0530

    Sixth commit

--- test/test_app.py
+++ test/test_app.py
@@ -4,7 +4,7 @@ from fastapi.testclient import TestClient
 client = TestClient(app)
 
 def test_app():
-    data = {"features": []}
+    data = {"features": [1,56,"Male",212801.73,30322.86,1,847,0]}
     response = client.post('/predict', json=data)
 
     assert response.status_code==200
